17f04cdafdde1d79939d3e260be8ef09a8615885
admin <yjl8628@naver.com>
2024-04-07 14:12:19 +0900

    cors 설정

--- src/main/java/spark/serverV2/Config.java
+++ src/main/java/spark/serverV2/Config.java
@@ -2,9 +2,19 @@ package spark.serverV2;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
-public class Config {
-
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .maxAge(3600);
+    }
 }
